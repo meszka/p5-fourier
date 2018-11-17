ecd6441ae599f837fac5b77b5a36d0d0c425b5f7
Jakub Szwachła <meszkax@gmail.com>
2018-11-17 12:56:22 +0100

    Use const and let instead of var

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
-var freqSlider;
-var fSelect;
-var f;
+let freqSlider;
+let fSelect;
 
 function setup() { 
   createCanvas(400, 800);
@@ -15,7 +14,7 @@ function setup() {
   fSelect.position(20, 20);
 }
 
-var funcs = {
+const funcs = {
   cos5hz: (t) => Math.cos(t * (2 * Math.PI) * 5),
   cos5hzplus1: (t) => Math.cos(t * (2 * Math.PI) * 5) + 1,
   cos2plus5hz: (t) => Math.cos(t * 2 * Math.PI * 2) + Math.cos(t * 2 * Math.PI * 5),
@@ -30,24 +29,24 @@ function ftPoint(f, freq) {
 
 function draw() { 
   background(255);
-  var freq = freqSlider.value();
-  f = funcs[fSelect.value()];
+  const freq = freqSlider.value();
+  const f = funcs[fSelect.value()];
   stroke(0);
   fill(0);
   text(freq, 300, 750);
-  var ftp = ftPoint(f, freq);
-  var integral = math.bignumber('0');
+  const ftp = ftPoint(f, freq);
+  const integral = math.bignumber('0');
 
   stroke(0);
   noFill();
   beginShape();
   lastT = 0;
-  for (var i = 0; i < 2000; i++) {
-    var t = i / 1000;
-    var z = ftp(t);
-    var x = (z.re + 1) * 80 + 80;
-    var y = (1 - z.im) * 80 + 480;
-    var dt = t - lastT;
+  for (let i = 0; i < 2000; i++) {
+    const t = i / 1000;
+    const z = ftp(t);
+    const x = (z.re + 1) * 80 + 80;
+    const y = (1 - z.im) * 80 + 480;
+    const dt = t - lastT;
     integral = math.add(integral, math.multiply(dt, z));
     lastT = t;
     vertex(x, y)
@@ -60,11 +59,11 @@ function draw() {
   stroke(0);
   noFill();
   beginShape();
-  for (var i = 1; i < 200; i++) {
-    var t = i/100;
-    var x = t * 200 + 80;
-    var ft = math.complex(f(t)).re;
-    var y = (1-ft) * 50 + 200;
+  for (let i = 1; i < 200; i++) {
+    const t = i/100;
+    const x = t * 200 + 80;
+    const ft = math.complex(f(t)).re;
+    const y = (1-ft) * 50 + 200;
     vertex(x, y);
   }
   endShape();
